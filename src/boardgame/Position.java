d574c83b16be42be3add06a0d80ded23c3c999d9
Rafael Garcia <rafaelsfs@gmail.com>
2021-05-05 23:13:33 -0300

    First Class: Position

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -22,7 +22,7 @@ public class Position {
         return column;
     }
 
-    public void setColum(int column) {
+    public void setColumn(int column) {
         this.column = column;
     }
     
